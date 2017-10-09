1b8083e3d361331c7bd9283bd606479081a7b97d
Douglas Brain <butterbrain77@gmail.com>
2017-10-09 09:43:22 +0100

    ADC voltage back to 3v from 5v

--- Fishmaster.X/mcc_generated_files/mcc.h
+++ Fishmaster.X/mcc_generated_files/mcc.h
@@ -50,15 +50,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "interrupt_manager.h"
-#include "fvr.h"
+#include "pwm3.h"
+#include "eusart.h"
+#include "pwm2.h"
+#include "pwm1.h"
+#include "dac1.h"
 #include "adc.h"
 #include "i2c.h"
-#include "pwm2.h"
-#include "pwm3.h"
-#include "dac1.h"
-#include "pwm1.h"
 #include "tmr2.h"
-#include "eusart.h"
+#include "fvr.h"
 
 #define _XTAL_FREQ  8000000
 
