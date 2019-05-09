cfd1ebf17d2c960122b0d3b7a85116faa5cd11ca
Douglas Brain <butterbrain77@gmail.com>
2019-05-09 17:39:02 +0100

    progress view showing bars and numbers

--- StaveInvaders/app/src/main/java/uk/co/darkerwaters/staveinvaders/views/GameProgressView.java
+++ StaveInvaders/app/src/main/java/uk/co/darkerwaters/staveinvaders/views/GameProgressView.java
@@ -4,8 +4,12 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.LinearGradient;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
 import android.text.TextPaint;
 import android.util.AttributeSet;
@@ -21,6 +25,9 @@ public class GameProgressView extends View {
 
     private Paint outlinePaint;
     private Paint letterPaint;
+    private Paint xAxisPaint;
+    private Paint barPaint;
+
     private Game game;
 
     public GameProgressView(Context context) {
@@ -52,6 +59,15 @@ public class GameProgressView extends View {
         this.letterPaint.setColor(getResources().getColor(R.color.colorLaser));
         this.letterPaint.setAntiAlias(true);
         this.letterPaint.setTextSize(36f);
+
+        this.barPaint = new Paint();
+        this.barPaint.setAntiAlias(true);
+        this.barPaint.setStyle(Paint.Style.FILL);
+
+        this.xAxisPaint = new Paint();
+        this.xAxisPaint.setAntiAlias(true);
+        this.xAxisPaint.setStrokeWidth(getResources().getDimension(R.dimen.letter_stroke));
+
     }
 
     public void setViewData(Game game) {
@@ -80,31 +96,57 @@ public class GameProgressView extends View {
         // draw the game count
         String gameCount = "0/" + this.game.children.length;
         float letterLeft = contentWidth - border - letterPaint.measureText(gameCount);
+        this.letterPaint.setTextScaleX(1f);
         canvas.drawText(gameCount, letterLeft, contentHeight - border, letterPaint);
+
+
+        Rect textBounds = new Rect();
+        this.letterPaint.getTextBounds(gameCount, 0, gameCount.length() - 1, textBounds);
 
         float graphRight = letterLeft - border;
         float graphLeft = border;
-        float graphBottom = contentHeight - border;
-        float graphTop = border;
+        float graphTop = border + textBounds.height();
+        float graphBottom = contentHeight - (border * 2f);
         //canvas.drawLine(graphLeft, graphBottom, graphRight, graphBottom, outlinePaint);
         //canvas.drawLine(graphLeft, graphBottom, graphLeft, graphTop, outlinePaint);
+
+        // create the path for the x-axis path to draw
+        Path axisPath = new Path();
+        axisPath.moveTo(graphLeft, graphBottom);
+        axisPath.lineTo(graphRight, graphBottom);
+        axisPath.lineTo(graphRight, graphBottom + border);
+        axisPath.lineTo(graphLeft, graphBottom);
+        // and the shader to draw this nicely
+        this.xAxisPaint.setShader(new LinearGradient(graphLeft, graphBottom, graphRight, graphBottom + border, Color.WHITE, this.letterPaint.getColor(), Shader.TileMode.MIRROR));
+        // and draw it
+        canvas.drawPath(axisPath, xAxisPaint);
+
+        // now draw the parts of the graph to display
 
         if (this.game.children.length > 0) {
             // draw all the child progress in
             float entryWidth = (graphRight - graphLeft) / this.game.children.length;
             for (int i = 0; i < this.game.children.length; ++i) {
-                float progress = this.game.children[i].getGameProgress();
+                float progress = 1f;//this.game.children[i].getGameProgress();
                 float barHeight = (graphBottom - graphTop) * progress;
                 if (barHeight <= 0f) {
                     // show a line of nothing
                     barHeight = outlinePaint.getStrokeWidth();
                 }
-                canvas.drawRoundRect(graphLeft + i * entryWidth,
+                String progressText = "60";//this.game.getTopTempo();
+
+                RectF barRect = new RectF(graphLeft + i * entryWidth,
                         graphBottom - barHeight,
-                        graphLeft + i * entryWidth + (0.9f * entryWidth),
-                        graphBottom,
-                        border, border,
-                        outlinePaint);
+                        graphLeft + i * entryWidth + (0.8f * entryWidth),
+                        graphBottom);
+                // create the shader
+                this.barPaint.setShader(new LinearGradient(barRect.left, barRect.bottom, barRect.right, barRect.top, this.outlinePaint.getColor(), Color.WHITE, Shader.TileMode.MIRROR));
+                canvas.drawRoundRect( barRect, border, border, this.barPaint);
+
+                String topBpm = "60"; //this.game.getTopTempo();
+                this.letterPaint.setTextScaleX(1f);
+                this.letterPaint.setTextScaleX(Math.min(barRect.width() / letterPaint.measureText(topBpm), 1f));
+                canvas.drawText(topBpm, barRect.left, barRect.top - textBounds.height() * 0.5f, letterPaint);
             }
         }
 
